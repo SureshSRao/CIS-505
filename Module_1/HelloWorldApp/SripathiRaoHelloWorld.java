c7dd25c7571acfcb70f6e557ffaaa3fba158985c
SureshSRao <ssripathirao@my365.bellevue.edu>
2025-09-08 22:06:01 -0500

    Added Module_1

---
+++ Module_1/HelloWorldApp/SripathiRaoHelloWorld.java
@@ -0,0 +1,10 @@
+public class SripathiRaoHelloWorld {
+    public static void main(String[] args) {
+        // Declare String variables for first and last name
+        String firstName = "Suresh";
+        String lastName = "Sripathi Rao";
+        
+        // Output using printf
+        System.out.printf("Hello World from %s %s.%n", firstName, lastName);
+    }
+}
