282f32d70210bfea08512849a068b0294a7c55a9
SureshSRao <ssripathirao@my365.bellevue.edu>
2025-09-08 22:25:00 -0500

    Added Module_1

--- Module_1/HelloWorldApp/SripathiRaoHelloWorld.java
+++ Module_1/HelloWorldApp/SripathiRaoHelloWorld.java
@@ -1,10 +1,17 @@
 /*
-    CIS505-T301 Intermediate Java Programming. Bellevue University.
     Assignment 1.2
-    Modified by Suresh Sripathi Rao 2025
+    Sripathi Rao S 2025 CIS505-T301 Intermediate Java Programming. Bellevue University.   
 */
 
 public class SripathiRaoHelloWorld {
+
+    /**
+     * The entry point of the program.
+     * This method initializes first and last name variables,
+     * then prints a greeting message to the console using printf.
+     *
+     * @param args Command-line arguments (not used in this program).
+     */
     public static void main(String[] args) {
         // Declare String variables for first and last name
         String firstName = "Suresh";
